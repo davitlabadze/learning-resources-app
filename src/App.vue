299a8dc699ec3fc3281fcf3da6a9e003da9ce176
davitlabadze <labadzedato18@gmail.com>
2022-04-20 10:24:52 +0400

    feat:component props

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
     <ul>
+        <learning-resource 
+        v-for="res in storeResources" 
+        :key="res.id" 
+        :title="res.title" 
+        :description="res.description"
+        :link="res.link"
+        ></learning-resource>
     </ul>
 </template>
 
 <script>
+    import LearningResource from './components/learning-resources/LerningResource.vue'
 export default {
+
+    components: {
+        LearningResource
+    },
     data(){
         return {
             storeResources: [

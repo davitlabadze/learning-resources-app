e8287701e0f7d8049cade828641672e82ee7dcda
davitlabadze <labadzedato18@gmail.com>
2022-04-20 10:31:59 +0400

    style: add styles to existing files

--- src/App.vue
+++ src/App.vue
@@ -38,3 +38,19 @@ export default {
 }
 
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
